7e256c8368f91e34ba3c803a93572de49dd53e57
Brad Hodge <bradley_hodge@hotmail.com>
2025-09-23 07:47:19 -0400

    fix: updated file extension to jsx

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>,
+);
+
