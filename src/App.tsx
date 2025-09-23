9e7a200a27fc3a2cbd5c212264058fb6938cdea7
Brad Hodge <bradley_hodge@hotmail.com>
2025-09-23 14:53:19 -0400

    fix: adjustment to global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,55 @@
-import React, {useState} from 'react'
-import '../src/styles/styles.css'
+import React, { useState } from "react";
 
-import Header from './components/Header'
-import SideSkirts from './components/SideSkirts'
-import Hero from './components/Hero'
-import Projects from './components/Projects'
-import About from './components/About'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import {StyledMain} from './components/styled/App.styled'
-import {StyledMainDark} from './components/styled/AppDark.styled'
+import Header from "./components/Header";
+import SideSkirts from "./components/SideSkirts";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { StyledMain } from "./components/styled/App.styled";
+import { StyledMainDark } from "./components/styled/AppDark.styled";
 
+export default function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [darkMode, setDarkMode] = useState("false");
 
-export default function App(){
+  window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
 
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-    const [darkMode, setDarkMode] = useState('false');
+  function setDarkModeToggle() {
+    darkMode === "false" ? setDarkMode("true") : setDarkMode("false");
+  }
 
-    window.addEventListener('resize', () => setWindowWidth(window.innerWidth))
+  const AppElements = () => {
+    return (
+      <>
+        <Header
+          windowWidth={windowWidth}
+          darkMode={darkMode}
+          onclick={setDarkModeToggle}
+        />
+        <SideSkirts darkMode={darkMode} />
+        <Hero darkMode={darkMode} />
+        <Projects darkMode={darkMode} />
+        <About darkMode={darkMode} />
+        <Contact darkMode={darkMode} />
+        <Footer darkMode={darkMode} />
+      </>
+    );
+  };
 
-    function setDarkModeToggle(){
-        darkMode === 'false' ? setDarkMode('true') : setDarkMode('false');
-    }
-
-    const AppElements = () =>{
-        return(
-            <>
-                <Header windowWidth={windowWidth} darkMode={darkMode} onclick={setDarkModeToggle} />
-                <SideSkirts darkMode={darkMode} />
-                <Hero darkMode={darkMode} />
-                <Projects darkMode={darkMode} />
-                <About darkMode={darkMode} /> 
-                <Contact darkMode={darkMode} />
-                <Footer darkMode={darkMode} />
-            </>
-        )
-    }
-
-    return(
-        <>
-            {darkMode === 'false' ?
-                <StyledMain id='main'>
-                    <AppElements />
-                </StyledMain>
-            :
-                <StyledMainDark id='main'>
-                    <AppElements />
-                </StyledMainDark>
-            }
-        </>
-    )
+  return (
+    <>
+      {darkMode === "false" ? (
+        <StyledMain id="main">
+          <AppElements />
+        </StyledMain>
+      ) : (
+        <StyledMainDark id="main">
+          <AppElements />
+        </StyledMainDark>
+      )}
+    </>
+  );
 }
+
