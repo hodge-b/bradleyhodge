3fb8f1657d2f63994951bfd28a77cc16fd78641d
Brad Hodge <bradley_hodge@hotmail.com>
2025-09-23 17:52:55 -0400

    chore: added pre-commit hooks

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,12 @@ import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    ignores: ["dist/**", "build/**", "node_modules/**"],
+    ignores: [
+      "dist/**",
+      "build/**",
+      "node_modules/**",
+      "commitlint.config.cjs",
+    ],
   },
   eslint.configs.recommended,
   tseslint.configs.recommended,
